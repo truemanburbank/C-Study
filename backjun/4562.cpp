bbe56d07028d533640b1b71c53ee66cef494e953
truemanburbank <juxtap.se@gmail.com>
2024-11-25 19:35:31 +0900

    boj 4562 cpp

---
+++ backjun/4562.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    for(int i = 0 ; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        if(a < b)
+            cout << "NO BRAINS" << '\n';
+        else
+            cout << "MMM BRAINS" << '\n';
+    }
+}
