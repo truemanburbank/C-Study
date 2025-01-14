c5a521854d6fd20e83d6d9d83ab7276bccc4d507
truemanburbank <juxtap.se@gmail.com>
2025-01-14 10:20:09 +0900

    Update 2447.cpp

--- backjun/2447.cpp
+++ backjun/2447.cpp
@@ -4,21 +4,18 @@
 using namespace std;
 int N;
 char stars[2222][2222];
-// 접근
-// 
-// 우선 N = 3일 때 사각형을 찍어보자!
-// 
-// 그럼 N = n이라면?
-// 함수 식 생각
+
 void PrintStars(int n, int x, int y)
 {
-    if (n == 1) 
+    if (n == 1)
     {
         stars[x][y] = '*';
         return;
     }
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             if (i == 1 && j == 1)
                 continue;
             PrintStars(n / 3, x + n / 3 * i, y + n / 3 * j);
@@ -29,9 +26,6 @@ void PrintStars(int n, int x, int y)
 
 int main()
 {
-    // 데이터 입력 받음
-    //scanf("%d", &N);
-
     int n;
     cin >> n;
     for (int i = 0; i < n; i++)
