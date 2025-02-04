70c0ce33d0120ead6423b8925404728a00bc168f
truemanburbank <juxtap.se@gmail.com>
2025-02-04 09:18:13 +0900

    Boj 15664 cpp
    백트래킹

---
+++ backjun/15664.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+
+// N개 중에서, M개 뽑음
+int N, M;
+
+// n번째로 뽑은 수를 저장
+int picked[10];
+
+// 숫자 모음
+int numbers[10];
+
+// k번째 수를 중복으로 뽑았는지 확인
+bool isUsed[10];
+
+// k 번째로 저장할 수를 탐색 
+void Solve(int k)
+{
+    if (k == M)
+    {
+        for (int i = 0; i < M; i++)
+            cout << picked[i] << ' ';
+        cout << '\n';
+
+        return;
+    }
+
+    // 현재 depth에서 마지막으로 뽑은 숫자 저장
+    int tmp = 0;
+    for (int i = 0; i < N; i++)
+    {
+        // 1. 같은 인덱스인 수를 중복해서 뽑았거나,
+        // 2. 같은 수가 여러개 있을 경우 생기는 중복 수열을 피한다.
+        // 3. (1, 7) (7, 1)과 같은 중복을 막기 위해 
+        //    (k - 1번째, k 번째)일 경우, k - 1번째 수 > k번째수이면
+        //    패스한다.
+
+        if (isUsed[i] || tmp == numbers[i] || (k > 0 && picked[k - 1] > numbers[i])) continue;
+        isUsed[i] = 1;
+        picked[k] = numbers[i];
+        tmp = picked[k];
+        Solve(k + 1);
+        isUsed[i] = 0;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> numbers[i];
+    }
+   
+    // 사전순으로 찾기 위해 배열을 정렬
+    sort(numbers, numbers + N);
+
+    Solve(0);
+
+    return 0;
+}
